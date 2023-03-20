6d500a196df7dfa921aadd3f3522ad1c8bf3b004
UI369 <UI369x@gmail.com>
2023-03-20 07:39:19 -0700

    fix crash bug with Intro/intro not existing

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,58 +1,58 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'DAO Masons',
-  tagline: 'The World is Crumbling and we are here to pick up the pieces.',
-  favicon: 'img/favicon.ico',
+  title: "DAO Masons",
+  tagline: "The World is Crumbling and we are here to pick up the pieces.",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: "https://your-docusaurus-test-site.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'DAOmasons', // Usually your GitHub org/user name.
-  projectName: 'foundation-docs', // Usually your repo name.
+  organizationName: "DAOmasons", // Usually your GitHub org/user name.
+  projectName: "foundation-docs", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -62,58 +62,58 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/dao-masons.png',
+      image: "img/dao-masons.png",
       navbar: {
-        title: 'DAO Masons',
+        title: "DAO Masons",
         logo: {
-          alt: 'DAO Masons Logo',
-          src: 'img/dao-masons.png',
+          alt: "DAO Masons Logo",
+          src: "img/dao-masons.png",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'Intro/intro',
-            position: 'left',
-            label: 'Foundation Docs',
+            type: "doc",
+            docId: "Intro/about",
+            position: "left",
+            label: "Foundation Docs",
           },
           {
-            href: 'https://github.com/daomasons',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/daomasons",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Intro',
-                to: '/docs/Intro',
+                label: "Intro",
+                to: "/docs/Intro",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Discord',
-                href: 'https://discord.gg/AwkdDVr7',
+                label: "Discord",
+                href: "https://discord.gg/AwkdDVr7",
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/daomasons',
+                label: "Twitter",
+                href: "https://twitter.com/daomasons",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'GitHub',
-                href: 'https://github.com/daomasons',
+                label: "GitHub",
+                href: "https://github.com/daomasons",
               },
             ],
           },
